1404f0e1e12c737489dfec1d4de867aae9af4169
urana03 <uhackninja@gmail.com>
2022-12-01 08:55:32 +0530

    Add:Program with starting position of player

---
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+	
+	public static int start = 0;
+	public static void main(String [] args){
+		System.out.println("Starting position of Player is :" + start);
+	}
+}
