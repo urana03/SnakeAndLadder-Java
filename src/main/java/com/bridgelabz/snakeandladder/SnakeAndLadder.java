8408b38fde60c27820a0d1d6bdaa525330fd6954
urana03 <uhackninja@gmail.com>
2022-12-01 12:00:17 +0530

    Add:Program to play snake and ladder between two players

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -8,39 +8,65 @@ public class SnakeAndLadder {
 	public static final int noPlay = 0;
 	public static void main(String [] args){
 		Random ran = new Random();
-		int position = 0;
 		int counter = 0;
-		System.out.println("Starting position of Player is :" + start);
-		
-		while(position<100) {
-		int die = ran.nextInt(6)+1;
-		System.out.println(die);
-		int condition = ran.nextInt(3);
-		
-		switch(condition) {
-		case 0:
-			System.out.println("It is no play");
-			System.out.println("Player Position:"+position);
-		break;
-		case 1:
-			System.out.println("It is a ladder");
-			position = position +die;
-			System.out.println("Player's position is "+position);
-		break;
-		case 2:
-			System.out.println("It's a snake");
-			position = position - die;
-			if(position<0) {
-				position = 0;
-			}
-			System.out.println("Player position: "+position);
-			break;
-		}
-		counter++;
-		
-	}
-		System.out.println("No of times die rolled: "+counter);
-        System.out.println("Player Won");
+		int position1 = 0;
+		int position2 = 0;
+		int turn = 0;
+		int oldturn = 0;
+		int condition;
+		System.out.println("Starting position of Snake and Ladder is :" + start);
+		while( position1 < 100 && position2 < 100){
+			if( turn == 0 )
+				System.out.println("Player 1 is playing");
+			else
+				System.out.println("Player 2 is playing");
+			int die = ran.nextInt(6)+1;
+			System.out.println("Player got : " + die);
+			condition = (int) (Math.floor(Math.random() * 10 ) %3 );
 
-	}
+			switch (condition)
+			{
+			case 0:
+				System.out.println("Its No Play player stays at same position. ");
+				break;
+			case 1:
+				System.out.println("Its  ladder. ");
+				if(turn == 0)
+					position1 += die;
+				else
+					position2 += die;
+				if(position1 > 100) {
+					position1 -= die;
+					System.out.println("Position of player 1 is exceding so not counting " );
+				}
+				if(position2 > 100){
+					position2 -= die;
+					System.out.println(" Position of player 2 is exceding so not counting  " );
+				}
+				turn += 1;
+				break;
+			case 2:		
+				System.out.println("Its snake. ");
+				if(turn == 0)
+					position1 -= die;
+				else
+					position2 -= die;
+				if(position1 < 0) 
+					position1 = 0;
+				if(position2 < 0) 
+					position2 = 0;
+				break;
+			}
+			counter++;
+			turn = (turn + 1 ) %2;
+			if(oldturn == 0)
+				System.out.println("Position of player 1 is : " + position1);
+			else
+				System.out.println("Position of player 2 is : " + position2);
+			oldturn = turn;	
+			System.out.println("\n");
+		}
+		System.out.println("Number of dies is rolled : " + counter);
+		System.out.println("Player "+(turn + 1) + " is won.");
+}
 }
