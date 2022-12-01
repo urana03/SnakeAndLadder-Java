8274f6c7adf7e6ed58e3e57348a4cef001b1fe7a
urana03 <uhackninja@gmail.com>
2022-12-01 11:32:20 +0530

    Add:Program to check the player won or not

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -9,9 +9,38 @@ public class SnakeAndLadder {
 	public static void main(String [] args){
 		Random ran = new Random();
 		int position = 0;
-		int die = ran.nextInt(6)+1;
 		System.out.println("Starting position of Player is :" + start);
+		
+		while(position<100) {
+		int die = ran.nextInt(6)+1;
 		System.out.println(die);
-	
+		int condition = ran.nextInt(3);
+		
+		switch(condition) {
+		case 0:
+			System.out.println("It is no play");
+			System.out.println("Player Position:"+position);
+		break;
+		case 1:
+			System.out.println("It is a ladder");
+			position = position +die;
+			System.out.println("Player's position is "+position);
+		break;
+		case 2:
+			System.out.println("It's a snake");
+			position = position - die;
+			if(position<0) {
+				position = 0;
+				System.out.println("Player's position: "+position);
+			}
+			else {
+				System.out.println("Player's position: "+position);
+			}
+			System.out.println("Player's position is: "+position);
+		}	
+		
+	}
+        System.out.println("Player Won");
+
 	}
 }
