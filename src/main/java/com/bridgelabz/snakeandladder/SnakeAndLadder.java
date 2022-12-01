e1eb54debeff42e3841fe4ccff29059abbfb976f
urana03 <uhackninja@gmail.com>
2022-12-01 09:25:27 +0530

    Add:Program to roll a die for a player

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -12,29 +12,6 @@ public class SnakeAndLadder {
 		int die = ran.nextInt(6)+1;
 		System.out.println("Starting position of Player is :" + start);
 		System.out.println(die);
-		int condition = ran.nextInt(3);
-		
-		switch(condition) {
-		case 0:
-			System.out.println("It is no play");
-			System.out.println("Player Position:"+position);
-		break;
-		case 1:
-			System.out.println("It is a ladder");
-			position = position +die;
-			System.out.println("Player's position is "+position);
-		break;
-		case 2:
-			System.out.println("It's a snake");
-			position = position - die;
-			if(position<0) {
-				position = 0;
-				System.out.println("Player's position: "+position);
-			}
-			else {
-				System.out.println("Player's position: "+position);
-			}
-		}
-		
+	
 	}
 }
